280fe34eefdf4d82373e313f67f5eca54045ecc9
DreamFlyer ( Li Xiang ) <lxx741@126.com>
2022-02-24 15:54:45 +0800

    2_24_google_fixed
    000

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ from utils.dice_score import dice_loss
 from evaluate import evaluate
 from unet import UNet
 
-dir_img = Path('./data/imgs/')
-dir_mask = Path('./data/masks/')
+dir_img = Path('./drive/MyDrive/u-net/data/imgs/')
+dir_mask = Path('./drive/MyDrive/u-net/data/masks/')
 dir_checkpoint = Path('./checkpoints/')
 
 
